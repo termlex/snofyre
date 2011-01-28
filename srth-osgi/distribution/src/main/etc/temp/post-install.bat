608c1d2124f24da5febabcfc6827c28daf1bec5c
jay.kola <jay.kola@29ca23ef-472f-bbfa-ca66-17e9de3a230d>
2011-01-28 17:18:49 +0000

    [maven-release-plugin]  copy for tag uk.nhs.cfh.dsp.snomed-0.8

--- srth-osgi/distribution/src/main/etc/temp/post-install.bat
+++
@@ -1,4 +0,0 @@
-# -- post installation scripts for clean up and configuration -- #
-@ECHO OFF
-start $INSTALL_PATH/docs/${sappheiros.configuration.guide.name}
-# start docs/${sappheiros.configuration.guide.name}
