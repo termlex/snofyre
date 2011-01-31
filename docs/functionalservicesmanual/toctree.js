8724279390877d6860b60f1979ca11c1adf2ac53
jay.kola <jay.kola@29ca23ef-472f-bbfa-ca66-17e9de3a230d>
2011-01-31 21:23:12 +0000

    Initial import of functional services manual

---
+++ docs/functionalservicesmanual/toctree.js
@@ -0,0 +1,63 @@
+
+      var tree;
+      
+      function treeInit() {
+      tree = new YAHOO.widget.TreeView("treeDiv1");
+      var root = tree.getRoot();
+    
+    
+    
+    
+    
+    
+    var objd4e53 = { label: "Introduction", href:"topics/functional-services-manual-introduction.html", target:"contentwin" };
+    var d4e53 = new YAHOO.widget.TextNode(objd4e53, root, false);var objd4e78 = { label: "Notice", href:"topics/snofyre-generic-notice.html", target:"contentwin" };
+    var d4e78 = new YAHOO.widget.TextNode(objd4e78, d4e53, false);var objd4e103 = { label: "License", href:"topics/open-government-license.html", target:"contentwin" };
+    var d4e103 = new YAHOO.widget.TextNode(objd4e103, d4e53, false);var objd4e128 = { label: "What is Snofyre?", href:"topics/snofyre-overview.html", target:"contentwin" };
+    var d4e128 = new YAHOO.widget.TextNode(objd4e128, d4e53, false);
+    var objd4e154 = { label: "Getting Started", href:"topics/user-manual-getting-started.html", target:"contentwin" };
+    var d4e154 = new YAHOO.widget.TextNode(objd4e154, root, false);var objd4e179 = { label: "Audience", href:"topics/audience.html", target:"contentwin" };
+    var d4e179 = new YAHOO.widget.TextNode(objd4e179, d4e154, false);var objd4e204 = { label: "System Requirements", href:"topics/systemRequirements.html", target:"contentwin" };
+    var d4e204 = new YAHOO.widget.TextNode(objd4e204, d4e154, false);var objd4e229 = { label: "Reporting Issues", href:"tasks/reporting-issues.html", target:"contentwin" };
+    var d4e229 = new YAHOO.widget.TextNode(objd4e229, d4e154, false);var objd4e254 = { label: "How this manual is organised", href:"topics/functional-services-manual-preface.html", target:"contentwin" };
+    var d4e254 = new YAHOO.widget.TextNode(objd4e254, d4e154, false);
+
+    var objd4e280 = { label: "Architecture Overview", href:"topics/application-framework-introduction.html", target:"contentwin" };
+    var d4e280 = new YAHOO.widget.TextNode(objd4e280, root, false);var objd4e305 = { label: "Snofyre Application Framework Overview", href:"topics/application-framework-overview.html", target:"contentwin" };
+    var d4e305 = new YAHOO.widget.TextNode(objd4e305, d4e280, false);
+    var objd4e332 = { label: "Services Overview", href:"topics/snofyre-core-services-overview.html", target:"contentwin" };
+    var d4e332 = new YAHOO.widget.TextNode(objd4e332, root, false);var objd4e357 = { label: "SNOMED CT Modules and Services", href:"topics/snomed-services-overview.html", target:"contentwin" };
+    var d4e357 = new YAHOO.widget.TextNode(objd4e357, d4e332, false);var objd4e382 = { label: "Snofyre Query Modules and Services", href:"topics/query-services-overview.html", target:"contentwin" };
+    var d4e382 = new YAHOO.widget.TextNode(objd4e382, d4e332, false);var objd4e407 = { label: "Search Services and Modules", href:"topics/search-services-overview.html", target:"contentwin" };
+    var d4e407 = new YAHOO.widget.TextNode(objd4e407, d4e332, false);
+    var objd4e433 = { label: "SNOMED CT Modules and Services", href:"topics/snomed-services-overview.html", target:"contentwin" };
+    var d4e433 = new YAHOO.widget.TextNode(objd4e433, root, false);var objd4e458 = { label: "SNOMED CT Object Model Module", href:"topics/snomed-object-model.html", target:"contentwin" };
+    var d4e458 = new YAHOO.widget.TextNode(objd4e458, d4e433, false);var objd4e486 = { label: "SNOMED CT Expression Model Module", href:"topics/snomed-expression-model.html", target:"contentwin" };
+    var d4e486 = new YAHOO.widget.TextNode(objd4e486, d4e433, false);var objd4e514 = { label: "SNOMED CT Data Access Module (DAO Module)", href:"topics/snomed-dao-service.html", target:"contentwin" };
+    var d4e514 = new YAHOO.widget.TextNode(objd4e514, d4e433, false);var objd4e542 = { label: "SNOMED CT Expression Comparator Service", href:"topics/snomed-expression-comparator-service.html", target:"contentwin" };
+    var d4e542 = new YAHOO.widget.TextNode(objd4e542, d4e433, false);var objd4e570 = { label: "SNOMED CT Normaliser Service", href:"topics/snomed-normaliser-service.html", target:"contentwin" };
+    var d4e570 = new YAHOO.widget.TextNode(objd4e570, d4e433, false);var objd4e604 = { label: "SNOMED CT Hierarchy Provider Service", href:"topics/snomed-hierarchy-provider-service.html", target:"contentwin" };
+    var d4e604 = new YAHOO.widget.TextNode(objd4e604, d4e433, false);var objd4e651 = { label: "SNOMED CT MRCM Service", href:"topics/snomed-mrcm-service.html", target:"contentwin" };
+    var d4e651 = new YAHOO.widget.TextNode(objd4e651, d4e433, false);var objd4e682 = { label: "SNOMED CT Expression Renderer Service", href:"topics/snomed-expression-renderer-service.html", target:"contentwin" };
+    var d4e682 = new YAHOO.widget.TextNode(objd4e682, d4e433, false);var objd4e716 = { label: "SNOMED CT Expression Converter Service", href:"topics/snomed-expression-converter-service.html", target:"contentwin" };
+    var d4e716 = new YAHOO.widget.TextNode(objd4e716, d4e433, false);
+    var objd4e754 = { label: "Snofyre Query Modules and Services", href:"topics/query-services-overview.html", target:"contentwin" };
+    var d4e754 = new YAHOO.widget.TextNode(objd4e754, root, false);var objd4e779 = { label: "Snofyre Query Model Module", href:"topics/snofyre-query-model.html", target:"contentwin" };
+    var d4e779 = new YAHOO.widget.TextNode(objd4e779, d4e754, false);var objd4e807 = { label: "Snofyre Query Transformer Service", href:"topics/snofyre-query-transformer-service.html", target:"contentwin" };
+    var d4e807 = new YAHOO.widget.TextNode(objd4e807, d4e754, false);var objd4e841 = { label: "Snofyre Query DAO Service", href:"topics/snofyre-query-dao-service.html", target:"contentwin" };
+    var d4e841 = new YAHOO.widget.TextNode(objd4e841, d4e754, false);var objd4e872 = { label: "Snofyre Data Generator Service", href:"topics/snofyre-data-generator-service.html", target:"contentwin" };
+    var d4e872 = new YAHOO.widget.TextNode(objd4e872, d4e754, false);var objd4e900 = { label: "Snofyre Information Model Module", href:"topics/snofyre-information-model-service.html", target:"contentwin" };
+    var d4e900 = new YAHOO.widget.TextNode(objd4e900, d4e754, false);var objd4e928 = { label: "Snofyre Demographics Model Module", href:"topics/snofyre-demographics-service.html", target:"contentwin" };
+    var d4e928 = new YAHOO.widget.TextNode(objd4e928, d4e754, false);
+    var objd4e957 = { label: "Search Services and Modules", href:"topics/search-services-overview.html", target:"contentwin" };
+    var d4e957 = new YAHOO.widget.TextNode(objd4e957, root, false);var objd4e982 = { label: "SNOMED CT Search Service", href:"topics/snomed-search-service.html", target:"contentwin" };
+    var d4e982 = new YAHOO.widget.TextNode(objd4e982, d4e957, false);
+    
+    
+    
+
+      tree.draw(); 
+      } 
+      
+      YAHOO.util.Event.addListener(window, "load", treeInit); 
+    
